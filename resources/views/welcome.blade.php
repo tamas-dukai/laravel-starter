57a7d4f7695eddaec83aa089734df6b505ff1f38
Tamas <tamas.dukai@gmail.com>
2021-09-29 18:05:57 +0200

    Added responsive styles to headings

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,6 @@
 
 @endpush
 
-
-
     @section('content')
 
         <div class="container">
@@ -57,6 +55,15 @@
                     <h6>This is a long, long, long, long, long, long, long, long, long, long, long, long, long, long, long, long, long, long H6 title!</h6>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quisquam excepturi, dolor laudantium nostrum vitae aspernatur, iusto, temporibus debitis architecto adipisci inventore voluptatem vel suscipit quasi. Velit eveniet unde hic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quisquam excepturi, dolor laudantium nostrum vitae aspernatur, iusto, temporibus debitis architecto adipisci inventore voluptatem vel suscipit quasi. Velit eveniet unde hic.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quisquam excepturi, dolor laudantium nostrum vitae aspernatur, iusto, temporibus debitis architecto adipisci inventore voluptatem vel suscipit quasi. Velit eveniet unde hic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quisquam excepturi, dolor laudantium nostrum vitae aspernatur, iusto, temporibus debitis architecto adipisci inventore voluptatem vel suscipit quasi. Velit eveniet unde hic.</p>
+
+                    <div class="mt-5 mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                      </div>
+                      <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                      </div>
                 </div>
             </div>
         </div>
